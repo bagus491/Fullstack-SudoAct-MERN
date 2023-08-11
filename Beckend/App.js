40277d0e5ed0b324f3b664a71138ae5c695f4eb6
bagus491 <baguskalam491@gmail.com>
2023-08-11 08:46:50 +0700

    setup(Beckend) ==> App.js first

---
+++ Beckend/App.js
@@ -0,0 +1,19 @@
+//first-setup
+const express = require('express');
+const app  = express();
+const port =  5000;
+
+
+
+
+
+
+
+
+
+
+
+//first-setup
+app.listen(port,() => {
+    console.log(`server berjalan di port ${port}`)
+});
