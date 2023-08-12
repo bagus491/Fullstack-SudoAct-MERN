893eb6ba1ec88ab8ebffc3840f967e9db998966e
bagus491 <baguskalam491@gmail.com>
2023-08-12 13:43:02 +0700

    Changess(Beckend) ==> WhistlistController ==> setup GetWhist,PostWhist,UpdateWhist,DeleteWhist UserRouter.js done

--- Beckend/src/Router/UserRouter.js
+++ Beckend/src/Router/UserRouter.js
@@ -7,6 +7,8 @@ const {CheckedToken,CheckedTokenLogin,CheckedTokenLogout} = require('../Utils/In
 const Auth = require('../Auth/Auth')
 //ProfileControllers
 const {CheckProfile,ProfilePost} = require('../Controllers/ProfileController')
+//Whistlist
+const {GetWhislists,PostWhislist,UpdateWhislist,DeleteWhislist} = require('../Controllers/WhislistController')
 
 //multer
 const multer = require('multer')
@@ -20,7 +22,19 @@ const storage = multer.diskStorage({
     }
 })
 
+//storageWhislist
+const WsStorage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null, '../public/Image/Uploads/Whislist')
+    },
+    filename: function(req,file,cb){
+        cb(null,Date.now()+'-'+file.originalname)
+    }
+})
+
 const Uploads = multer({storage})
+//UploadsWs
+const UploadsWs = multer({storage: WsStorage})
 
 //router GET
 app.get('/',HomeWeb)
@@ -34,8 +48,16 @@ app.get('/dasbord/profile/:username',CheckProfile)
 //PostProfile
 app.post('/dasbord/profile/:username',Uploads.single('Profile'),ProfilePost)
 
-//whislist
+//whislistPage
 app.get('/whistlist/:username',CheckedToken)
+//WhistlistCard
+app.get('/whistlist/card/:username',GetWhislists)
+//postWhislist
+app.post('/whistlist/card/:username',UploadsWs.single('Avatar'),PostWhislist)
+//updateWhist
+app.put('/whistlist/card/:username/:id',UploadsWs.single('Avatar'),UpdateWhislist)
+//delete
+app.delete('/whistlist/card/:username/:id',DeleteWhislist)
 
 //notepad
 app.get('/notepad/:username',CheckedToken)
