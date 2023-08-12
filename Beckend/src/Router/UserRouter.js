23e653a69a602f2c07bcb89339c8b222a2c355b2
bagus491 <baguskalam491@gmail.com>
2023-08-12 14:19:58 +0700

    Changess(Beckend) ==> NotepadController ==> setup GetPad,PostPad,UpdatePad,DeletePad,DetailPad UserRouter.js done

--- Beckend/src/Router/UserRouter.js
+++ Beckend/src/Router/UserRouter.js
@@ -9,6 +9,8 @@ const Auth = require('../Auth/Auth')
 const {CheckProfile,ProfilePost} = require('../Controllers/ProfileController')
 //Whistlist
 const {GetWhislists,PostWhislist,UpdateWhislist,DeleteWhislist} = require('../Controllers/WhislistController')
+//Notepad
+const {GetPad,GetDetailPad,GetPostPad,GetUpdatePad,GetDeletePad}  = require('../Controllers/NotepadController')
 
 //multer
 const multer = require('multer')
@@ -61,6 +63,19 @@ app.delete('/whistlist/card/:username/:id',DeleteWhislist)
 
 //notepad
 app.get('/notepad/:username',CheckedToken)
+//NotepadCard
+app.get('/notepad/card/:username',GetPad)
+//details
+app.get('/notepad/card/detail/:username/:id',GetDetailPad)
+
+//post
+app.post('/notepad/card/:username',GetPostPad)
+
+//put
+app.put('/notepad/card/:username/:id',GetUpdatePad)
+
+//delete
+app.delete('/notepad/card/:username/:id',GetDeletePad)
 
 
 //setting
