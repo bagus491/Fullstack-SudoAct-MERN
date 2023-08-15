bab7ebc3252f52f929b4b08f02efab014b5b7e7c
bagus491 <baguskalam491@gmail.com>
2023-08-15 17:28:02 +0700

    setup(404 NOT FOUND) ==> FrontEnd done

--- frontend/src/Pages/NotFoundPage.jsx
+++ frontend/src/Pages/NotFoundPage.jsx
@@ -4,7 +4,18 @@
 export const NotFoundPage = () => {
     return(
         <>
-        <h1>NotFound</h1>
+        <div className="bg-404">
+        <section class="notFound">
+        <div class="img">
+        <img src="https://assets.codepen.io/5647096/backToTheHomepage.png" alt="Back to the Homepage"/>
+        <img src="https://assets.codepen.io/5647096/Delorean.png" alt="El Delorean, El Doc y Marti McFly"/>
+        </div>
+        <div class="text">
+        <h1>404</h1>
+        <h2>PAGE NOT FOUND</h2>
+        </div>
+    </section>
+        </div>
         </>
     )
 }
